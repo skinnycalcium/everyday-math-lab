941e55401f96a90afc3490f50d253f92182a4a95
skinnycalcium <121703381+skinnycalcium@users.noreply.github.com>
2025-05-13 14:56:11 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js
+
+// Import Firebase services
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhspD3jqwF7iS9ypg6JQszvH2Fn3I4hc_4",
+  authDomain: "everyday-math-lab.firebaseapp.com",
+  projectId: "everyday-math-lab",
+  storageBucket: "everyday-math-lab.appspot.com",
+  messagingSenderId: "955525594831",
+  appId: "1:955525594831:web:25c849c25b03df21dec1ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
